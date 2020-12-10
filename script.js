b02533348ab9dc9ba3b4a303292765e94c4dc32e
Jelmerovereem <jelmer_overeem@hotmail.com>
2020-12-10 12:33:23 +0100

    created links between nodes

--- script.js
+++ script.js
@@ -1,35 +1,97 @@
 // nodes.js
-nodes.NetworkNodes.Nodes.Node.forEach(node => {
+const ExampleNode = nodes.NetworkNodes.Nodes.Node[9];
+console.log(ExampleNode);
+for (const [key, value] of Object.entries(ExampleNode)) {
+  document.querySelector(".example-node").insertAdjacentHTML("beforeend", `<p><strong>${key}:</strong> ${value}</p>`)
+  if (key === "DisplayProperties") {
+    for (const [nestedKey, nestedValue] of Object.entries(value)) {
+      document.querySelector(".example-node").insertAdjacentHTML("beforeend", `<p style="margin-left: 100px;"><strong>${nestedKey}:</strong>${nestedValue}</p>`)
+    }
+   }
+  if (key === "Attributes") {
+    value.Attribute.forEach((attr) => {
+      document.querySelector(".example-node").insertAdjacentHTML("beforeend", `<p style="margin-left: 100px;"><strong>${attr.Name}:</strong> ${attr.Value}</p>`)
+    })
+  }
+}
+
+//document.querySelector(".example-node");
+
+
+nodes.NetworkNodes.Nodes.Node.forEach((node, index) => {
+  console.log(index, node)
   let nodeRow = `
     <tr>
+      <td>${index}</td>
       <td>${node.Label}</td>
+      <td>${node.SID}</td>
       <td>${node.NodeID}</td>
       <td>${node.NodeType}</td>
-      <td>${node.SubNodeID}</td>
+      <td>${node.SubNodeID}<img src="assets/${node.SubNodeID}.png" alt=""></td>
     </tr>
   `;
 	document.querySelector(".allNodes table tbody").insertAdjacentHTML("beforeend", nodeRow);
 });
 
-console.log(anb)
-anb.Chart.ChartItemCollection.ChartItem.forEach(ChartItem => {
-  console.log(ChartItem)
-  var iCOVNodeID = "";
-  if (Array.isArray(ChartItem.AttributeCollection.Attribute)) {
-    iCOVNodeID = ChartItem.AttributeCollection.Attribute.find(obj => obj._AttributeClassReference === "iCOV_node_id")._Value;
-    if (iCOVNodeID === undefined) {
-      iCOVNodeID = "Onbekend";
-    }
+let nodesArray = nodes.NetworkNodes.Nodes.Node;
+
+
+edges.NetworkEdges.EdgeSets.EdgeSet.forEach((line, index) => {
+  /*console.log(index, line)*/
+  if (Array.isArray(line.EdgesInSet.Edge)) {
+    var fromNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge[0].FromNodeSID
+    });
+    var fromNodeLabel = fromNodeObj.Label;
+    var fromNodeIcon = fromNodeObj.SubNodeID;
+
+    var toNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge[0].ToNodeSID
+    });
+    var toNodeLabel = toNodeObj.Label;
+    var toNodeIcon = toNodeObj.SubNodeID;
+
+
+    var SecondFromNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge[1].FromNodeSID
+    });
+    var SecondFromNodeLabel = SecondFromNodeObj.Label;
+    var SecondFromNodeIcon = SecondFromNodeObj.SubNodeID;
+
+    var SecondToNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge[1].ToNodeSID
+    });
+    var SecondToNodeLabel = SecondToNodeObj.Label;
+    var SecondToNodeIcon = SecondToNodeObj.SubNodeID;
+
+    var firstConnection = line.EdgesInSet.Edge[0].Label;
+
+    var secondConnection = line.EdgesInSet.Edge[1].Label;
   } else {
-    iCOVNodeID = "Onbekend"
+    var fromNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge.FromNodeSID
+    });
+    var fromNodeLabel = fromNodeObj.Label;
+    var fromNodeIcon = fromNodeObj.SubNodeID;
+
+    var toNodeObj = nodesArray.find((obj) => {
+      return obj.SID === line.EdgesInSet.Edge.ToNodeSID
+    });
+    var toNodeLabel = toNodeObj.Label;
+    var toNodeIcon = toNodeObj.SubNodeID;
+
+    var firstConnection = line.EdgesInSet.Edge.Label;
   }
-  let ChartItemRow = `
+
+  let lineHtml = `<p>${fromNodeLabel} ---> ${toNodeLabel}</p>`;
+  let tableRowHtml = `
     <tr>
-      <td>${ChartItem._Label}</td>
-      <td>${iCOVNodeID}</td>
-      <td><img src="assets/${ChartItem.End.Entity.Icon.IconStyle._Type}.png" alt="">${ChartItem.End.Entity.Icon.IconStyle._Type}</td>
-      <td></td>
+      <td>${index}</td>
+      <td>${fromNodeLabel}<img src="assets/${fromNodeIcon}.png" alt=""><br><br>${SecondFromNodeLabel}<img src="assets/${SecondFromNodeIcon}.png" alt=""></td>
+      <td>${toNodeLabel}<img src="assets/${toNodeIcon}.png" alt=""><br><br>${SecondToNodeLabel}<img src="assets/${SecondToNodeIcon}.png" alt=""></td>
+      <td>${firstConnection}<br><br>${secondConnection}</td>
     </tr>
   `;
-  document.querySelector(".anb table tbody").insertAdjacentHTML("beforeend", ChartItemRow);
-});
+  //document.querySelector(".edges").insertAdjacentHTML("beforeend", lineHtml)
+  document.querySelector(".network table tbody").insertAdjacentHTML("beforeend", tableRowHtml);
+})
